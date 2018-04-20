c3f5120811b9ff2ffedf9391eef9211b6bb3a93b
liyue2018 <1059869380@qq.com>
2018-04-20 16:24:58 +0800

    jquery.lazyload.js

--- lazyload.js
+++ lazyload.js
@@ -2,5 +2,48 @@
 * @Author: liyue2018
 * @Date:   2018-04-18 14:00:25
 * @Last Modified by:   liyue2018
-* @Last Modified time: 2018-04-18 14:00:25
+* @Last Modified time: 2018-04-20 15:14:59
 */
+function isInSight(el) {
+  const bound = el.getBoundingClientRect();
+  const clientHeight = window.innerHeight;
+  //如果只考虑向下滚动加载
+  //const clientWidth=window.innerWeight;
+  return bound.top <= clientHeight + 100;
+}
+
+let index = 0;
+function checkImgs() {
+  const imgs = document.querySelectorAll('.my-photo');
+  for (let i = index; i < imgs.length; i++) {
+    if (isInSight(imgs[i])) {
+      loadImg(imgs[i]);
+      index = i;
+    }
+  }
+}
+
+function loadImg(el) {
+  if (!el.src) {
+    const source = el.dataset.src;
+    el.src = source;
+  }
+}
+
+function throttle(fn, mustRun = 500) {
+  const timer = null;
+  let previous = null;
+  return function() {
+    const now = new Date();
+    const context = this;
+    const args = arguments;
+    if (!previous) {
+      previous = now;
+    }
+    const remaining = now - previous;
+    if (mustRun && remaining >= mustRun) {
+      fn.apply(context, args);
+      previous = now;
+    }
+  }
+}
